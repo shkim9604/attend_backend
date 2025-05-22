7dff1c62e11cbc61f20c47cba1e85b1fcf81cc0b
shkim9604@naver.com <shkim9604@naver.com>
2025-05-23 03:40:20 +0900

    User모델추가및 회원가입,로그인기능 추가

--- User/models.py
+++ User/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+#유저
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    employee_number = models.IntegerField()
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return  f'{self.name} - {self.department_code} - {self.id_number}'
+
+    class Meta:
+        ordering = ['-id_number']
+        db_table = "users"
--- User/views.py
+++ User/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+import json
 
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import User
 # Create your views here.
+
+#회원가입
+def SignUp(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        name = data.get('name','')
+        employee_number = data.get('employee_number','')
+        phone_number = data.get("phone_number",'')
+
+        user = User.objects.get(name=name, employee_number=employee_number)
+
+        if user is not None: #이미 있다면 가입된유저이다
+            return JsonResponse({'message': '이미 가입된 상태입니다.'})
+
+        #user가 None이면 미가입유저이기에 user를 생성해서 가입시킨다.
+        User.objects.create(
+            name = name,
+            employee_number = employee_number,
+            phone_number = phone_number
+        )
+
+        return JsonResponse({'success': True, 'message': '가입에 성공하였습니다.'})
+    else:
+        return JsonResponse({'success': False, 'message': '잘못된 요청입니다.'})
+
+#로그인
+def Login(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        name = data.get('name','')
+        employee_number = data.get('employee_number')
+
+        #UserDB에서 사용자 확인
+        user = User.objects.get(name=name, employee_number=employee_number)
+        if user is not None:
+            #사용자 있음
+            return JsonResponse({'success': True, 'name': user.name, 'employee_number': user.employee_number})
+        else:
+            #사용자 없음
+            return JsonResponse({'success': False, 'message': "로그인 실패\n이름이나 사원번호를 확인해주세요."})
+    else:
+        return JsonResponse({'success': False, 'message': '잘못된 요청 방식입니다.'})
