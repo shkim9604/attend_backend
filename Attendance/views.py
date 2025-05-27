d8ded0226921e2e1464ff29939a312ba7e56546d
shkim9604@naver.com <shkim9604@naver.com>
2025-05-28 04:24:50 +0900

    출장추가로인한 모델수정및출장출발,복귀 기능추가

--- Attendance/views.py
+++ Attendance/views.py
@@ -6,6 +6,50 @@ import json
 
 # Create your views here.
 
+def business_start(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        name = data.get('name','')
+        employee_number = data.get('employee_number','')
+        check_date = data.get('check_date','')
+        business_start_time = data.get('buiness_start_time','')
+        business_start_place= data.get('place_name','')
+        #날짜,시간 변형
+        check_date = datetime.strptime(check_date,"%Y-%m-%d").date()
+        business_start_time = datetime.strptime(business_start_time, "%H:%M").time()
+        Attendance.objects.create(
+            name = name,
+            employee_number = employee_number,
+            check_date = check_date,
+            business_start_time = business_start_time,
+            business_start_place = business_start_place,
+        )
+        return JsonResponse({'success': True, 'message': f'{business_start_time}에 출장출발 처리 되었습니다.'})
+    else:
+        return JsonResponse({'success': False, 'message': "잘못된 요청입니다."})
+def business_end(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        name = data.get('name','')
+        employee_number = data.get('employee_number', '')
+        check_date = data.get('check_date', '')
+        business_end_time = data.get('business_end_time','')
+        business_end_place = data.get('place_name','')
+        #날짜,시간 형태변경
+        check_date = datetime.strptime(check_date,"%Y-%m-%d").date()
+        business_end_time = datetime.strptime(business_end_time, "%H:%M").time()
+        #가장최근기록에 복귀 데이터 넣기
+        recent_attendance = Attendance.objects.filter(name=name,employee_number=employee_number).order_by('-check_date','-create_time')
+        recent_attendance = recent_attendance[0]
+        recent_attendance.business_end_time = business_end_time
+        recent_attendance.business_end_place = business_end_place
+        recent_attendance.save()
+
+        return JsonResponse({'success': True, 'message': f'{business_end_time}에 출장복귀 처리 되었습니다.'})
+    else:
+        return JsonResponse({'success': False, 'message': "잘못된 요청입니다."})
+
+
 def check_in(request):
     if request.method == "POST":
         data = json.loads(request.body)
